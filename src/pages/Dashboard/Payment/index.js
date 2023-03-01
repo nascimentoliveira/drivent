167a6b411c4f34fd9bcf628b2a2153702a49271a
luskamusca <luskamuskammouse@gmail.com>
2023-03-01 12:11:36 -0300

    feat: components and enrollment required message

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,14 +1,60 @@
 import Splash from '../../../components/Splash';
+import Text from '../../../components/Text';
 import useEnrollment from '../../../hooks/api/useEnrollment';
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+import Button from '../../../components/Button';
 
 export default function Payment() {
   const { enrollment, enrollmentLoading } = useEnrollment();
-  if (!enrollment) {
-    return 'Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso';
-  }
   if (enrollmentLoading) {
     return <Splash loading />;
   }
+  if (!enrollment) {
+    return (
+      <>
+        <StyledTypography variant="h4">Inscrição e pagamento</StyledTypography>
+        <AlignBox>
+          <TextBox>
+            <Text text={'Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso'}></Text>
+          </TextBox>
+        </AlignBox>
+      </>
+    );
+  }
 
-  return 'Pagamento: Em breve!';
+  return (
+    <>
+      <StyledTypography variant="h4">Inscrição e pagamento</StyledTypography>
+      <Text text={'Primeiro, escolha sua modalidade de ingresso'}></Text>
+      <AlignBox2>
+        <Button text1={'Presencial'} text2={'R$ 200'}></Button>
+        <Button text1={'Online'} text2={'R$ 150'}></Button>
+      </AlignBox2>
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
+
+const TextBox = styled.div`
+width:388px;
+height: 46px;
+text-align: center;
+`;
+
+const AlignBox = styled.div`
+height: 440px;
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+const AlignBox2 = styled.div`
+margin-top: 17px;
+display: flex;
+flex-direction: row;
+align-items: center;
+`;
