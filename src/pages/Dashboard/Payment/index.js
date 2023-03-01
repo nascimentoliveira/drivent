6131c3317f32760e9811a73ef79d0708f9dda98e
Raísa Peixoto <raisacad.engcivil@gmail.com>
2023-03-01 12:56:02 -0300

    merge main

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,19 @@
+import { useState } from 'react';
+import Splash from '../../../components/Splash';
+import useEnrollment from '../../../hooks/api/useEnrollment';
+import TicketTypes from './TicketTypes';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const { enrollment, enrollmentLoading } = useEnrollment();
+  const [isReserved, setIsReserved] = useState(false);
+  if (enrollmentLoading) {
+    return <Splash loading />;
+  }
+  if (!enrollment) {
+    return 'Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso';
+  }
+  if (isReserved) {
+    return <> Página de pagamento</>;
+  }
+  return <TicketTypes />;
 }
---
+++ src/services/ticketTypesApi.js
@@ -0,0 +1,12 @@
+import api from './api';
+
+export async function getTicketTypes(token) {
+  const response = await api.get('/tickets/types', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+ 
+  return response.data;
+}
+//
